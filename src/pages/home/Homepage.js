637c004968b37ad0c16db9ab89d57019675caedc
Sachin Sharma <sachin@gryt.fit>
2022-09-15 18:38:38 +0530

    INTIAL Setup

---
+++ src/pages/home/Homepage.js
@@ -0,0 +1,10 @@
+import React from 'react';
+
+// Styles here
+import "./styles.scss";
+
+export default function Homepage() {
+  return (
+    <div className='homepage-style'>Homepage</div>
+  )
+}
